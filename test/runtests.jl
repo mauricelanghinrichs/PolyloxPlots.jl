062a77fbc5fd6b12cd08453cc4c69d4da4fc6eb3
mauricelanghinrichs <m.langhinrichs@icloud.com>
2023-11-02 18:51:43 +0100

    test | heatmap

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,12 +1,50 @@
 
 using PolyloxPlots
-using Random
+using DataFrames
 using Test
 
-Random.seed!(1)
-
 @testset "Heatmap" begin
-    d = 5
-    @test all((0, 100) .<= d .<= (1, 101))
+    df = DataFrame(:Barcode => 1:10,
+                :A => [10,0,0,1,3,5,3,1,2,5],
+                :B => [1,8,4,0,0,0,0,3,2,1],
+                :C => [10,0,1,1,5,5,7,1,0,5])
 
+    @test plxheatmap(df) isa Any
+    # @test_nowarn plxheatmap(df)
+
+    dfres = deepcopy(df)
+    PolyloxPlots.dfheatmap!(dfres, x -> x)
+
+    # show(df, allrows=true)
+    # show(dfres, allrows=true)
+
+    sortinds = sortperm(dfres.Barcode)
+    @test propertynames(df) == [:Barcode, :A, :B, :C]
+    @test propertynames(dfres) == [:Barcode, :A, :C, :B]
+    @test df.Barcode == 1:10
+    @test dfres.Barcode == [2, 3, 9, 4, 5, 6, 7, 8, 10, 1]
+    @test df.Barcode == dfres.Barcode[sortinds]
+    @test df.A == dfres.A[sortinds]
+    @test df.B == dfres.B[sortinds]
+    @test df.C == dfres.C[sortinds]
+
+    dfres = deepcopy(df)
+    PolyloxPlots.dfheatmap!(dfres, x -> x==zero(x) ? -1 : log(x))
+
+    # show(df, allrows=true)
+    # show(dfres, allrows=true)
+
+    @test propertynames(df) == [:Barcode, :A, :B, :C]
+    @test propertynames(dfres) == [:Barcode, :A, :C, :B]
+    @test df.Barcode == 1:10
+    @test dfres.Barcode == [2, 3, 9, 4, 5, 6, 7, 8, 10, 1]
+    
+    sortinds = sortperm(dfres.Barcode)
+    @test all(dfres.A[sortinds][df.A .== 0] .== -1)
+    @test all(dfres.B[sortinds][df.B .== 0] .== -1)
+    @test all(dfres.C[sortinds][df.C .== 0] .== -1)
+
+    @test all(dfres.A[sortinds][df.A .!= 0] .≈ log.(df.A[df.A .!= 0]))
+    @test all(dfres.B[sortinds][df.B .!= 0] .≈ log.(df.B[df.B .!= 0]))
+    @test all(dfres.C[sortinds][df.C .!= 0] .≈ log.(df.C[df.C .!= 0]))
 end
