53d011675d439a4fd35cc5eef54d1652d63ecf74
mauricelanghinrichs <m.langhinrichs@icloud.com>
2023-11-03 19:37:19 +0100

    code | heatmap update

--- src/plxheatmap.jl
+++ src/plxheatmap.jl
@@ -14,7 +14,12 @@ function dfheatmap!(df, valtransform)
     select!(df, vcat(:Barcode, celltypes))
 
     # reorder barcode rows, first by fate, within fate groups by sum
-    df[!, :Fate] = [[v > 0.0 for v in row] for row in eachrow(df[:, celltypes])]
+    fatesdf = [[v > 0.0 for v in row] for row in eachrow(df[:, celltypes])]
+    fatesunique = unique(fatesdf)
+    sort!(fatesunique, by = x -> (sum(x), x), rev=false)
+    allfates = Dict(fatesunique .=> eachindex(fatesunique))
+
+    df[!, :Fate] = [allfates[fate] for fate in fatesdf]
     df[!, :Sum] = sum(eachcol(df[:, celltypes]))
 
     sort!(df, [:Fate, :Sum], rev=[false, false])
@@ -31,11 +36,11 @@ end
 
 Creates a heatmap of barcode clone sizes for all celltypes in the dataframe (second to last column).
 
-Celltypes are reordered based on a hierarchical clustering. Barcodes are ordered in groups of same fate combinations, and within these groups via the sum of counts over all celltypes for a given barcode.
+Celltypes are reordered based on a hierarchical clustering with Kendall rank correlation. Barcodes are ordered in groups of same fate combinations, and within these groups via the sum of counts over all celltypes.
 
 # Arguments
 - `df::DataFrame`: a barcode dataframe, first column (`:Barcode`) contains barcodes, while all further columns contain barcode reads / cell counts of the celltypes.
-- `valtransform = x -> x==zero(x) ? -1 : log(x)`: function used to transform barcode read / cell count values (values for the colorbar).
+- `valtransform = x -> x==zero(x) ? -1 : log(x)`: function that transforms barcode read / cell count values (transformed values used for the colorbar).
 - `resolution = (200,250)`: figure resolution / size.
 
 # Examples
@@ -46,6 +51,7 @@ julia> df = DataFrame(:Barcode => 1:10,
                         :B => [1,8,4,0,0,0,0,3,2,1],
                         :C => [10,0,1,1,5,5,7,1,0,5])
 julia> fig, ax, hm = plxheatmap(df)
+julia> fig # display figure
 julia> save("plxheatmap.pdf", fig)
 ```
 """
