180969c0375914c4fbb051b440f137e7d21ff40f
mauricelanghinrichs <m.langhinrichs@icloud.com>
2023-11-03 19:46:05 +0100

    test | bubble chart

--- test/runtests.jl
+++ test/runtests.jl
@@ -48,3 +48,37 @@ using Test
     @test all(dfres.B[sortinds][df.B .!= 0] .≈ log.(df.B[df.B .!= 0]))
     @test all(dfres.C[sortinds][df.C .!= 0] .≈ log.(df.C[df.C .!= 0]))
 end
+
+@testset "Bubble chart" begin
+    df = DataFrame(:Barcode => 1:6,
+                :Aaa => [1,0,0,0,1,0],
+                :Bbb => [0,0.5,0.5,0,1,2],
+                :Ccc => [0,0,0,1,1,2])
+
+    @test plxheatmap(df) isa Any
+    # @test_nowarn plxheatmap(df)
+
+    celltypenames = nothing
+    nallfates = 3
+    x, y, readfraction, yticks = PolyloxPlots.dfbubble(df, celltypenames, nallfates)
+    @test x == [1, 1, 2, 2, 2, 3, 3, 3]
+    @test y == [3, 7, 2, 4, 7, 1, 4, 7]
+    @test readfraction == [0.5, 0.5, 0.25, 0.5, 0.25, 0.25, 0.5, 0.25]
+    @test yticks == (1:1:7, ["C", "B", "A", "BC", "AC", "AB", "ABC"])
+
+    celltypenames = ["a", "b", "c"]
+    nallfates = 3
+    x, y, readfraction, yticks = PolyloxPlots.dfbubble(df, celltypenames, nallfates)
+    @test x == [1, 1, 2, 2, 2, 3, 3, 3]
+    @test y == [3, 7, 2, 4, 7, 1, 4, 7]
+    @test readfraction == [0.5, 0.5, 0.25, 0.5, 0.25, 0.25, 0.5, 0.25]
+    @test yticks == (1:1:7, ["c", "b", "a", "bc", "ac", "ab", "abc"])
+
+    celltypenames = ["a", "b", "c"]
+    nallfates = 2
+    x, y, readfraction, yticks = PolyloxPlots.dfbubble(df, celltypenames, nallfates)
+    @test x == [1, 1, 2, 2, 2, 3, 3, 3]
+    @test y == [3, 5, 2, 4, 5, 1, 4, 5]
+    @test readfraction == [0.5, 0.5, 0.25, 0.5, 0.25, 0.25, 0.5, 0.25]
+    @test yticks == (1:1:5, ["c", "b", "a", "bc", "abc"])
+end
