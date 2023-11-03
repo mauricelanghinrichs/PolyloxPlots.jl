6498ddccca91d1ec25f61b0501c625f2f5c94ace
mauricelanghinrichs <m.langhinrichs@icloud.com>
2023-11-03 12:27:56 +0100

    docs | template bubble

--- src/plxbubble.jl
+++ src/plxbubble.jl
@@ -1,4 +1,10 @@
 
+"""
+    plxbubble(prior, dist!, ϵ_target, varexternal; <keyword arguments>)
+
+Run ABC with diffential evolution (de) moves in a Sequential Monte Carlo setup (smc) 
+providing posterior samples and a model evidence estimate.
+"""
 function plxbubble(df::DataFrame;
                 resolution=(200,600))
 
